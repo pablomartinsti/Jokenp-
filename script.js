624e11487fd821dfe7605dd2c44d5a24bc610291
pablomartinsti <pablomartinsti@outlook.com>
2024-04-07 19:57:01 -0300

    comit

---
+++ script.js
@@ -0,0 +1,52 @@
+const response = document.querySelector('.response')
+const machinePoint = document.querySelector('.machinepoint')
+const humanPoint = document.querySelector('.humanpoint')
+
+
+let humanPoints = 0
+let machinePoints = 0
+
+const humanOption = (humanresult) => {
+
+    letsPlay(humanresult, computerOption())
+}
+
+
+const computerOption = () => {
+
+    const option = ['rock', 'paper', 'scissors']
+
+    const result = Math.floor(Math.random() * 3)
+
+    return option[result]
+
+
+}
+
+const letsPlay = (human, machine) => {
+
+    console.log(human, machine)
+
+    if (human === machine) {
+        response.innerHTML = "Deu Empate! 😕😕"
+
+    } else if (human === 'paper' && machine === 'rock' ||
+        human === 'rock' && machine === 'scissors' ||
+        human === 'scissors' && machine === 'paper'){
+
+            humanPoints++
+
+            humanPoint.innerHTML =humanPoints
+            response.innerHTML = "Você Ganhou! 🥳🎉"
+
+    }else {
+
+        machinePoints++
+        machinePoint.innerHTML=machinePoints
+        response.innerHTML = "Você Perdeu! 😔😔"
+    }
+
+
+
+
+}
